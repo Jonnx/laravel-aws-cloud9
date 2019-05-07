ba77bcf3cf61513f9cdc8fabf98cad98b788b8b7
Jonnx <weigert.jonas@gmail.com>
2019-05-07 02:03:37 +0000

    update php install script for 7.3 to include bcmath (for lavel nova)

--- php/7.3/install.sh
+++ php/7.3/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-LC_ALL=C.UTF-8 
+LC_ALL=C.UTF-8
 sudo add-apt-repository ppa:ondrej/php -y
 sudo apt-get update
 
@@ -29,4 +29,5 @@ sudo apt-get --force-yes install -qq \
     php7.3-xml \
     php7.3-sqlite \
     php7.3-soap \
-    php7.3-zip
+    php7.3-zip \
+    php7.3-bcmath
